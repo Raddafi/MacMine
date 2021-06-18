4d7aaeebc8a681b9e8c755cc81b85c752bdd3eb9
Raddafi <86112922+Raddafi@users.noreply.github.com>
2021-06-18 20:34:19 +0700

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -17,7 +17,7 @@ local EEPROMProxy, internetProxy, GPUProxy =
 GPUProxy.bind(getComponentAddress("screen"))
 local screenWidth, screenHeight = GPUProxy.getResolution()
 
-local repositoryURL = "https://raw.githubusercontent.com/IgorTimofeev/MacMine/main/"
+local repositoryURL = "https://raw.githubusercontent.com/Raddafi/MacMine/main/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
